dc9eb641c518eae8d62d445772e4248bb6f68274
yaser989 <45394628+yaser989@users.noreply.github.com>
2022-03-21 11:37:56 +0100

    first commit

---
+++ thaerProjet/Program.cs
@@ -0,0 +1,107 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+String firstName = "Thaer";
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine("First Name:" + firstName);
+
+String lastName = "Abbas";
+int age = 32;
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.Black;
+
+Console.WriteLine("Last Name:" + lastName + "\n" + "Age:" + age);
+
+var currentDate = DateTime.Now;
+Console.WriteLine("Curent Date:" + currentDate);
+
+// Declare variables and then initialize to zero.
+int num1 = 0;
+int num2 = 0;
+
+// Ask the user to type the first number.
+Console.WriteLine("Type a number, and then press Enter");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+// Ask the user to type the second number.
+Console.WriteLine("Type another number, and then press Enter");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 < num2)
+{
+    Console.WriteLine("The biggest number :" + num2);
+}
+else if (num2 == num1)
+{
+    Console.WriteLine("the numbers are aquals ");
+}
+else
+{
+    Console.WriteLine("The biggest number :" + num1);
+}
+
+Console.WriteLine("=======================================================================================");
+
+// Declare variables and then initialize to zero.
+int num3 = 20;
+int num4 = 0;
+int count = 0;
+
+// Ask the user to guiss the number.
+Console.WriteLine("Type a number to guiss wich number i chosse and then press Enter");
+
+do
+{
+    num4 = Convert.ToInt32(Console.ReadLine());
+    count++;
+    if (num4 == num3)
+    {
+        Console.WriteLine("Congratulations, you won after trying" + " " + count + " " + "times ");
+    }
+    else if (num4 < num3)
+    {
+        Console.WriteLine("The number was to Small");
+    }
+    else if (num4 > num3)
+    {
+        Console.WriteLine("The number was to large");
+    }
+} while (num4 != num3);
+
+Console.WriteLine("=======================================================================================");
+
+// Ask the user to Write a text.
+Console.WriteLine("write a text to save to the file");
+String text = Convert.ToString(Console.ReadLine());
+
+
+string path = @"D:\projet.txt";
+
+// Create a file to write to.
+string createText = text + Environment.NewLine;
+File.WriteAllText(path, createText);
+
+
+Console.WriteLine($"your file text saved to the file (projet.txt) inside {path} with SUCCESS");
+
+Console.WriteLine("===========================================================");
+
+Console.WriteLine("type o to open the file to read the text");
+// Open the file to read from.
+string readText = File.ReadAllText(path);
+
+if (Console.ReadKey().Key == ConsoleKey.O)
+{
+    Console.WriteLine("\n your text inside the file is:"+" "+readText);
+
+}
+
+
+
+Console.Write("\nPress 'Enter' to exit the process...");
+
+while (Console.ReadKey().Key != ConsoleKey.Enter )
+{
+}
+
